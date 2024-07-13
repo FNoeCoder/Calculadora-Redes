2bc8e17630bda558d841216166a2cdbadc8fe92f
Francisco Noé Arriaga <112121312+FNoeCoder@users.noreply.github.com>
2024-07-13 13:03:09 -0600

    Encale corregido

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://FNoeCoder.github.io/Calculadora-de-Subredes/',
+  base: 'https://fnoecoder.github.io/Calculadora-Redes/',
 })
